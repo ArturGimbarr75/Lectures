a3cc0f450fbcd0d520f5b1aff0f7c2bd85c23a6c
Artur <Artur@email.com>
2023-10-30 21:18:31 +0200

    Add project files.

---
+++ 001_Les/ColorConsole.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_Les;
+
+internal static class ColorConsole
+{
+	private static ConsoleColor[] _colors;
+	private static int _index = 0;
+
+	static ColorConsole()
+	{
+		_colors = Enum.GetValues(typeof(ConsoleColor))
+						.Cast<ConsoleColor>()
+						.Where(c => c != Console.BackgroundColor)
+						.ToArray();
+	}
+
+	public static void WriteLine(string text, bool separateByChar = false)
+	{
+		if (separateByChar)
+		{
+			foreach (var c in text)
+			{
+				Console.ForegroundColor = NextColor();
+				Console.Write(c);
+			}
+            Console.WriteLine();
+        }
+		else
+			foreach (var line in text.Split("\n"))
+			{
+				Console.ForegroundColor = NextColor();
+				Console.WriteLine(line);
+			}
+	}
+
+	public static void Write(string text, bool separateByChar = false)
+	{
+		if (separateByChar)
+			foreach (var c in text)
+			{
+				Console.ForegroundColor = NextColor();
+				Console.Write(c);
+			}
+		else
+			foreach (var line in text.Split("\n"))
+			{
+				Console.ForegroundColor = NextColor();
+				Console.Write(line);
+			}
+	}
+
+	private static ConsoleColor NextColor()
+	{
+		if (_index >= _colors.Length)
+		{
+			_index = 0;
+		}
+
+		return _colors[_index++];
+	}
+}
---
+++ 001_Les/Program.cs
@@ -0,0 +1,63 @@
+﻿using _001_Les;
+
+string triangle =
+"""
+   /\
+  /  \
+ /    \
+/______\
+""";
+
+string circle =
+"""
+     *****
+   **     **
+ **         **
+**   Hello   **
+**    from   **
+ **   C#    **
+   **     **
+     *****
+""";
+
+string heart =
+"""
+ /\     /\
+/  \   /  \
+\   \ /   /
+ \   V   /
+  \     /
+   \   /
+    \ /
+     V
+""";
+
+ConsoleColor[] triangleColors = new int[] { 14, 10, 12, 13 }.Cast<ConsoleColor>().ToArray();
+ConsoleColor[] heartColors = new int[] { 1, 9, 11, 14, 13, 12, 4, 10 }.Cast<ConsoleColor>().ToArray();
+
+Console.WriteLine($"{triangle}\n\n{circle}\n");
+foreach (var (l, c) in triangle.Split("\n").Zip(triangleColors))
+{
+	Console.ForegroundColor = c;
+	Console.WriteLine(l);
+}
+Console.WriteLine("\n");
+foreach (var (l, c) in heart.Split("\n").Zip(heartColors))
+{
+	Console.ForegroundColor = c;
+	Console.WriteLine(l);
+}
+
+ColorConsole.WriteLine("\nGood: Apple\nCount: 5\nPrice: 2 EU\n");
+
+int age = 28;
+ColorConsole.WriteLine(
+	$"""
+	Name: Some Name and Surname
+	Age: {age}
+	Work: Softare Developer
+	Email: name.surname@gmail.com
+	Phone: +1 123-456-7890
+	""", true);
+
+Console.ResetColor();

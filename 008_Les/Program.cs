fff34f5ac53834f986dd58edc5c771291ed28d8a
Artur <Artur@email.com>
2023-11-16 18:35:42 +0200

    Move class

--- 008_Les/Program.cs
+++ 008_Les/Program.cs
@@ -40,37 +40,3 @@
 		Console.WriteLine($"Cannot divide {number} by {divider}");
 	}
 }
-
-static class Task2
-{
-	public static void GetUserData(out string name, out string surname)
-	{
-		do
-		{
-			Console.Write("Enter your name: ");
-			name = Console.ReadLine()!;
-		}
-		while (name is null);
-
-		do
-		{
-            Console.Write("Enter you surname: ");
-            surname = Console.ReadLine()!;
-		}
-		while (surname is null);
-	}
-
-	public static bool TryDivide(double num, double divider, out double result)
-	{
-		if (divider == 0)
-		{
-			result = 0;
-			return false;
-		}
-		else
-		{
-			result = num / divider;
-			return true;
-		}
-	}
-}
---
+++ 008_Les/Task2.cs
@@ -0,0 +1,36 @@
+﻿// Task 1
+
+// Task2
+static class Task2
+{
+	public static void GetUserData(out string name, out string surname)
+	{
+		do
+		{
+			Console.Write("Enter your name: ");
+			name = Console.ReadLine()!;
+		}
+		while (name is null);
+
+		do
+		{
+            Console.Write("Enter you surname: ");
+            surname = Console.ReadLine()!;
+		}
+		while (surname is null);
+	}
+
+	public static bool TryDivide(double num, double divider, out double result)
+	{
+		if (divider == 0)
+		{
+			result = 0;
+			return false;
+		}
+		else
+		{
+			result = num / divider;
+			return true;
+		}
+	}
+}

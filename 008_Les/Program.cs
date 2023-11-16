39a82ef9de6696f9cdc8fb018a68fcfc106556ba
Artur <Artur@email.com>
2023-11-16 18:35:21 +0200

    Task 8

---
+++ 008_Les/Program.cs
@@ -0,0 +1,76 @@
+﻿// Task 1
+{
+	int a = 1, b = 2;
+	Console.WriteLine($"a = {a}, b = {b}");
+	Task1.Swap(ref a, ref b);
+	Console.WriteLine($"a = {a}, b = {b}");
+
+	double d1 = 1.0, d2 = 2.0;
+    Console.WriteLine($"d1 = {d1}, d2 = {d2}");
+	Task1.Add(ref d1, d2);
+	Console.WriteLine($"d1 = {d1}, d2 = {d2}");
+
+	string s = "  Hello, World!  ";
+	Console.WriteLine($"s = \"{s}\"");
+	Task1.TrimAndCapitalize(ref s);
+	Console.WriteLine($"s = \"{s}\"");
+}
+
+// Task2
+{
+	Task2.GetUserData(out string name, out string surname);
+	Console.WriteLine($"Hello, {name} {surname}!");
+
+	int num = 0;
+	do
+	{
+		Console.Write("Enter a number less than 100: ");
+
+	}
+	while (!int.TryParse(Console.ReadLine(), out num) || num >= 100);
+
+	double number = 5;
+	double divider = 0;
+	if (Task2.TryDivide(number, divider, out double result))
+	{
+		Console.WriteLine($"{number} / {divider} = {result}");
+	}
+	else
+	{
+		Console.WriteLine($"Cannot divide {number} by {divider}");
+	}
+}
+
+static class Task2
+{
+	public static void GetUserData(out string name, out string surname)
+	{
+		do
+		{
+			Console.Write("Enter your name: ");
+			name = Console.ReadLine()!;
+		}
+		while (name is null);
+
+		do
+		{
+            Console.Write("Enter you surname: ");
+            surname = Console.ReadLine()!;
+		}
+		while (surname is null);
+	}
+
+	public static bool TryDivide(double num, double divider, out double result)
+	{
+		if (divider == 0)
+		{
+			result = 0;
+			return false;
+		}
+		else
+		{
+			result = num / divider;
+			return true;
+		}
+	}
+}
---
+++ 008_Les/Task1.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+static class Task1
+{
+	public static void Swap<T> (ref T a, ref T b) => (a, b) = (b, a);
+
+	public static void Add<T> (ref T a, T b) where T : IAdditionOperators<T, T, T>
+	{
+		a += b;
+	}
+
+	public static void TrimAndCapitalize(ref string s)
+	{
+		s = s.Trim().ToUpper();
+	}
+}

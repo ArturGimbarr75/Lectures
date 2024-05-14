ee8480d807c9809b2966e714b98f6802c999ff90
Artur <Artur@email.com>
2024-05-14 22:09:52 +0300

    Add DTOs

---
+++ NoteExam.DTO/User.cs
@@ -0,0 +1,8 @@
+﻿namespace NoteExam.DTO;
+
+public class User
+{
+	public string Username { get; set; } = string.Empty;
+	public string Password { get; set; } = string.Empty;
+	public string RepeatPassword { get; set; } = string.Empty;
+}

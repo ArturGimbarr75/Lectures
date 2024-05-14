770a0d8460d8654fc30bf3d626879c566d2e499f
Artur <Artur@email.com>
2024-05-14 22:12:42 +0300

    Add migration

--- NoteExam.Models/User.cs
+++ NoteExam.Models/User.cs
@@ -6,4 +6,6 @@ public class User
 	public string Username { get; set; } = string.Empty;
 	public string PasswordHash { get; set; } = string.Empty;
 	public string PasswordSalt { get; set; } = string.Empty;
+	public string JwtToken { get; set; } = string.Empty;
+	public string RefreshToken { get; set; } = string.Empty;
 }
---
+++ NoteExam.Persistence/Migrations/20240514191215_AddTokens.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NoteExam.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTokens : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "JwtToken",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JwtToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "Users");
+        }
+    }
+}
